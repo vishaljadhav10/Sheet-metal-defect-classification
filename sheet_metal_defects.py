a8d67b748ccea2f870246ef2549ae231925d4baa
vishaljadhav10 <135214466+vishaljadhav10@users.noreply.github.com>
2023-06-01 14:22:22 +0530

    Update sheet_metal_defects.py

--- sheet_metal_defects.py
+++ sheet_metal_defects.py
@@ -58,6 +58,8 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Conv2D(64, (2,2), activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
+     tf.keras.layers.Conv2D(64, (2,2), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation='relu'),
     tf.keras.layers.Dropout(0.2),
